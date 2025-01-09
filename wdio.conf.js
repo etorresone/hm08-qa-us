51aef30a48ec6ddb49220190813e5efa132e9d7b
eltorres <elia.torresone@gmail.com>
2025-01-09 12:28:27 -0800

    updating

--- wdio.conf.js
+++ wdio.conf.js
@@ -28,7 +28,7 @@ exports.config = {
 ],
     logLevel: 'error',
     bail: 0,
-    baseUrl: 'https://e629ed6b-4969-442b-987e-087e81eb10ca.serverhub.practicum-services.com',
+    baseUrl: 'https://cnt-728c8369-ff4c-4233-88a1-1d97c0e8ab95.containerhub.tripleten-services.com',
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
